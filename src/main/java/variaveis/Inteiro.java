502a8a718802e0d87119a428bc714787854f09e8
Jorge-Cammarota <Jorge-Cammarota>
2023-09-17 16:47:05 -0300

    concatenar

---
+++ src/main/java/variaveis/Inteiro.java
@@ -0,0 +1,9 @@
+package variaveis;
+
+public class Inteiro {
+    public static void main(String[] args) {
+    int idade = 13 * 8;
+
+    System.out.println("multiplicacao da idade 13 pela idade 8 é " + idade);
+    }
+}
